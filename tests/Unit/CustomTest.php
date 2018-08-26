1acc202d5a4169ca54d66cca2c7392dc5f44e084
Mishal Alsharekh <mishal.sharekh@gmail.com>
2018-08-27 00:55:09 +0300

    first Test  --echo "\nconnected sucessfully to database ";

---
+++ tests/Unit/CustomTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CustomTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    // public function testExample()
+    // {
+    //     $this->assertTrue(true);
+    // }
+
+    public function testIsConnected()
+    {
+        $isConnected=false;
+        # code...
+        if(DB::connection()->getDatabaseName()){
+            $isConnected=true;
+            echo "\nconnected sucessfully to database ";
+            
+        }
+
+        $this->assertTrue($isConnected);
+    }
+}
